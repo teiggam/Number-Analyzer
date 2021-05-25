f8b270dc0860121b10fd362290727c2dc9b734bf
maggie <maggie.tamanini@gmail.com>
2021-05-25 15:16:49 -0400

    Cleaned up code to reflect new material from class and made the responses more entertaining.

--- Program.cs
+++ Program.cs
@@ -9,57 +9,68 @@ namespace Number_Analyzer
             bool choice = true;
 
             string userName;
-            Console.WriteLine("Hello! What is your name?");
+            Console.WriteLine("Hello! I'm Numberella! What is your name?");
             userName = Console.ReadLine();
-            Console.WriteLine("");
-            Console.WriteLine("Hi, " + userName + ", welcome to the number analyzer game!");
+            Console.WriteLine();
+            Console.WriteLine("Hi, " + userName + ", welcome to my number analyzer game!");
 
-            while (choice)
+            while (choice == true)
             {
-                int a;
-
-
                 Console.WriteLine("");
                 Console.WriteLine(userName + ", please enter a whole number between 1 and 100.");
-                a = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("");
-
-                if (a < 101 && a > 0)
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+                int input = int.Parse(userInput);
+                if (input < 101 && input > 0)
                 {
-                    Console.WriteLine("Thanks so much, " + userName + ", for entering a number between 1 and 100.");
-                    Console.WriteLine("");
+                    Console.WriteLine("Thanks so much, " + userName + ", for correctly entering a number between 1 and 100.");
+                    Console.WriteLine();
                 }
                 else
                 {
-                    Console.WriteLine("That is not a number between 1 and 100, " + userName + ", but I can still analyze the number for you!");
-                    Console.WriteLine("");
+                    Console.WriteLine("Oh," + userName + ", you and I both know that is not what I asked for... but I'll analyze it anyway, out of the kindness of my number-loving heart.");
+                    Console.WriteLine();
                 }
 
-                if (a % 2 == 0)
-                    if (a < 25)
+                if (input % 2 == 0)
+                    if (input < 25)
                     {
-                        Console.WriteLine(a + ", even and less than 25.");
+                        Console.WriteLine("The number " + input + " is even and less than 25.");
                     }
                     else
                     {
-                        Console.WriteLine(a + ", even.");
+                        Console.WriteLine("The number " + input + " is even.");
                     }
-
                 else
-                    Console.WriteLine(a + ", odd.");
+                {
+                    Console.WriteLine("The number " + input + " is odd.");
+                }
 
-
-
-                Console.WriteLine("");
+                Console.WriteLine();
                 Console.WriteLine("Want to check more numbers? (y/n): ");
+                string answer = Console.ReadLine();
 
-
-                if (Console.ReadLine() == "n")
+                if (answer.ToLower() == "y")
+                {
+                    choice = true;
+                    continue;
+                }
+                else if (answer.ToLower() == "n")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("That's too bad, I was just getting warmed up!");
+                    Console.WriteLine("Thanks and have a wonderful day " + userName + "!");
                     choice = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("I don't understand, let me ask again.");
+                    continue;
+
+                }
 
             }
-            Console.WriteLine("");
-            Console.WriteLine("Thanks, have a wonderful day" + userName + "!");
         }
     }
 
